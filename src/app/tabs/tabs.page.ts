66ed2dece5ccbd8ace10f8bd8eedf2052918308f
medardomoranb <119766136+medardomoranb@users.noreply.github.com>
2024-12-04 10:32:02 -0500

    first

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,7 +1,8 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { triangle, ellipse, square } from 'ionicons/icons';
+import { triangle, ellipse, square, peopleCircle   } from 'ionicons/icons';
+
 
 @Component({
   selector: 'app-tabs',
@@ -14,6 +15,6 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ triangle, ellipse, square });
+    addIcons({ triangle, ellipse, square, peopleCircle });
   }
 }
