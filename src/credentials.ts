a0c69548f3743ba47f1aca5f9b92db30513e0171
medardomoranb <119766136+medardomoranb@users.noreply.github.com>
2024-12-09 10:51:19 -0500

    Lectura y escritura de Informacion

---
+++ src/credentials.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDQyONxDPGEqc86VNF_HhcBQTlKNf_fPhU",
+    authDomain: "hibrida-49f48.firebaseapp.com",
+    projectId: "hibrida-49f48",
+    storageBucket: "hibrida-49f48.firebasestorage.app",
+    messagingSenderId: "554336199591",
+    appId: "1:554336199591:web:94ce53aabacc82e73e0c82",
+    measurementId: "G-2MB57H32P7"
+};
